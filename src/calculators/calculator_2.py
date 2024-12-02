4d17fd93ce34bf4088fb7ad1f853755ae5151382
MyTruQs <raulfernandes.rm@gmail.com>
2024-12-01 22:26:44 -0300

    aula12

--- src/calculators/calculator_2.py
+++ src/calculators/calculator_2.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 from flask import request as FlaskRequest
-from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
 
 class Calculator2:
     """
@@ -10,6 +10,10 @@ class Calculator2:
     * Todos esses N numeros são multiplicados por 11 e elevados a pontência de 0.95.
     * Por fim, é retirado o desvio padrão desses resultados e retornado o inverso desse valor (1/result).
     """
+    
+    def __init__(self, driver_handler: DriverHandlerInterface) -> None:
+        self.__driver_handler = driver_handler
+    
     def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
         body = request.json
         input_data = self.__validate_body(body)
@@ -26,9 +30,8 @@ class Calculator2:
         return input_data
     
     def __process_data(self, input_data: List[float]) -> float:
-        numpy_handler = NumpyHandler()
         first_process_result = [(num * 11) ** 0.95 for num in input_data]
-        result = numpy_handler.standard_derivation(first_process_result)
+        result = self.__driver_handler.standard_derivation(first_process_result)
         return 1/result
     
     def __format_response(self, calculated_number: float) -> Dict:
