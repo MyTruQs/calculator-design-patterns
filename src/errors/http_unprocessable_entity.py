68d1deea45543c85d2c16f41bb8a341d0e69ca52
MyTruQs <raulfernandes.rm@gmail.com>
2024-12-04 15:39:42 -0300

    aula20

--- src/errors/http_unprocessable_entity.py
+++ src/errors/http_unprocessable_entity.py
@@ -1,4 +1,4 @@
-class HttpUnprocessableEntity(Exception):
+class HttpUnprocessableEntityError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
         self.message = message
